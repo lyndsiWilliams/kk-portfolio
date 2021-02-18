abd3b24db31ae98c235a930bb7f2f3318573c3fc
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-18 16:07:35 -0600

    Migration begin: App and Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,29 @@
+// React
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Components
+import NavBar from './components/NavBar';
+// import Home from './components/Home';
+// import About from './components/About';
+// import ShortStories from './components/ShortStories';
+// import Gallery from './components/Gallery';
+// import Blog from './components/Blog';
+// Styling
 import './App.css';
+// import './styles/styles.css';
 
 function App() {
   return (
     <div className="App">
-      Placeholder Homepage
+      <Router>
+        <NavBar />
+        {/* <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/short-stories" component={ShortStories} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/blog" component={Blog} />
+        </Switch> */}
+      </Router>
     </div>
   );
 }
