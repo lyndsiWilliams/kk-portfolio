08d5f2d1a40318834266c821f69bcd29bc52ec24
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-19 16:07:35 -0600

    Home component migrated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Components
 import NavBar from './components/NavBar';
-// import Home from './components/Home';
+import Home from './components/Home';
 // import About from './components/About';
 // import ShortStories from './components/ShortStories';
 // import Gallery from './components/Gallery';
@@ -16,13 +16,13 @@ function App() {
     <div className="App">
       <Router>
         <NavBar />
-        {/* <Switch>
+        <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          {/* <Route path="/about" component={About} />
           <Route path="/short-stories" component={ShortStories} />
           <Route path="/gallery" component={Gallery} />
-          <Route path="/blog" component={Blog} />
-        </Switch> */}
+          <Route path="/blog" component={Blog} /> */}
+        </Switch>
       </Router>
     </div>
   );
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Kathleen Kidder</h1>
+      <h2>Writer, artist, cat and lizard fanatic</h2>
+    </div>
+  );
+};
+
+export default Home;
