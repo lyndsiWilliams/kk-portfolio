ea26c2bba885b10fbf166437e1932371aacbabc4
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-20 22:45:02 -0600

    Gallery and Blog components migrated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,8 +5,8 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import ShortStories from './components/ShortStories';
-// import Gallery from './components/Gallery';
-// import Blog from './components/Blog';
+import Gallery from './components/Gallery';
+import Blog from './components/Blog';
 // Styling
 import './App.css';
 // import './styles/styles.css';
@@ -20,8 +20,8 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/short-stories" component={ShortStories} />
-          {/* <Route path="/gallery" component={Gallery} /> */}
-          {/* <Route path="/blog" component={Blog} /> */}
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/blog" component={Blog} />
         </Switch>
       </Router>
     </div>
---
+++ client/src/components/Blog.tsx
@@ -0,0 +1,9 @@
+const Blog = () => {
+  return (
+    <div className="blog" style={{ flexDirection: "column" }}>
+      <h1>This is the Blog page!</h1>
+    </div>
+  );
+};
+
+export default Blog;
