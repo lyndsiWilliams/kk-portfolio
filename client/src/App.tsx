59347917d402474644cc32c38b6bf474bc0edf01
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-20 22:40:30 -0600

    About component migrated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Components
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-// import About from './components/About';
+import About from './components/About';
 // import ShortStories from './components/ShortStories';
 // import Gallery from './components/Gallery';
 // import Blog from './components/Blog';
@@ -18,10 +18,10 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route path="/about" component={About} />
-          <Route path="/short-stories" component={ShortStories} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/blog" component={Blog} /> */}
+          <Route path="/about" component={About} />
+          {/* <Route path="/short-stories" component={ShortStories} /> */}
+          {/* <Route path="/gallery" component={Gallery} /> */}
+          {/* <Route path="/blog" component={Blog} /> */}
         </Switch>
       </Router>
     </div>
