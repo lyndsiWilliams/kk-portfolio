d38d1f7d98767eed710f2aca8aa16b22ae668fc2
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-20 22:41:48 -0600

    ShortStories component migrated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
-// import ShortStories from './components/ShortStories';
+import ShortStories from './components/ShortStories';
 // import Gallery from './components/Gallery';
 // import Blog from './components/Blog';
 // Styling
@@ -19,7 +19,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
-          {/* <Route path="/short-stories" component={ShortStories} /> */}
+          <Route path="/short-stories" component={ShortStories} />
           {/* <Route path="/gallery" component={Gallery} /> */}
           {/* <Route path="/blog" component={Blog} /> */}
         </Switch>
