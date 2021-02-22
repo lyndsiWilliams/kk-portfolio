56a0320785868123f53d714a94fa2f89be550a23
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-22 17:54:55 -0600

    Trying a new route, found a better way to set up GraphiQL

--- client/src/components/Blog.tsx
+++ client/src/components/Blog.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 const Blog = () => {
+  useEffect(() => {
+    axios.get('http://localhost:5309/graphql')
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error));
+  }, [])
+
   return (
     <div className="blog" style={{ flexDirection: "column" }}>
       <h1>This is the Blog page!</h1>
