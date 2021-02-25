c697c2fac2d0d0a68a6b3938bedbf2e17b7f9907
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-25 16:07:35 -0600

    Putting a hold on the mongodb for now

--- server.js
+++ server.js
@@ -2,111 +2,58 @@
 const express = require('express');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
-// const {
-//   GraphQLSchema,
-//   GraphQLObjectType,
-//   GraphQLNonNull,
-//   GraphQLInt,
-//   GraphQLString,
-//   GraphQLList
-// } = require('graphql');
 const gql = require('graphql-tag');
 const { buildASTSchema } = require('graphql');
-// Express app creation
-const app = express();
+const mongoose = require('mongoose');
 // Database
 const { POSTS } = require('./db.json');
-// Middleware
-app.use(cors());
 
-// // Type definitions
-// const PostType = new GraphQLObjectType({
-//   name: 'Post',
-//   description: 'This represents a post written by Katie',
-//   fields: () => ({
-//     id: { type: GraphQLNonNull(GraphQLInt) },
-//     title: { type: GraphQLNonNull(GraphQLString) },
-//     content: { type: GraphQLNonNull(GraphQLString) }
-//   })
-// });
+const startServer = async () => {
+  // Express app creation
+  const app = express();
+  // Middleware
+  app.use(cors());
 
-// const RootQueryType = new GraphQLObjectType({
-//   name: 'Query',
-//   description: 'Root Query',
-//   fields: () => ({
-//     post: {
-//       type: PostType,
-//       description: 'A single post',
-//       args: {
-//         id: { type: GraphQLInt }
-//       },
-//       resolve: (parent, args) => db.posts.find(post => post.id === args.id)
-//     },
-//     posts: {
-//       type: new GraphQLList(PostType),
-//       description: 'List of all posts',
-//       resolve: () => db.posts
-//     }
-//   })
-// });
+  // Mongoose setup
+  await mongoose.connect('mongodb://localhost:27017/test3', {useNewUrlParser: true});
 
-// const RootMutationType = new GraphQLObjectType({
-//   name: 'Mutation',
-//   description: 'Root mutation',
-//   fields: () => ({
-//     addPost: {
-//       type: PostType,
-//       description: 'Add a post',
-//       args: {
-//         title: { type: GraphQLNonNull(GraphQLString) },
-//         content: { type: GraphQLNonNull(GraphQLString) }
-//       },
-//       resolve: (parent, args) => {
-//         const post = {
-//           id: db.posts.length + 1,
-//           title: args.title,
-//           content: args.content
-//         };
-//         db.posts.push(post);
-//         return post;
-//       }
-//     }
-//   })
-// });
+  const Cat = mongoose.model('Cat', { name: String });
 
-// // GraphQL schema
-// const schema = new GraphQLSchema({
-//   query: RootQueryType,
-//   mutation: RootMutationType
-// });
+  const kitty = new Cat({ name: 'Zildjian' });
+  kitty.save().then(() => console.log('meow'));
 
-const schema = buildASTSchema(gql`
-  type Query {
-    posts: [Post]
-    post(id: ID!): Post
-  }
+  // GraphQL schema
 
-  type Post {
-    id: ID
-    title: String
-    content: String
-  }
-`);
+  const schema = buildASTSchema(gql`
+    type Query {
+      posts: [Post]
+      post(id: ID!): Post
+    }
 
-const mapPost = (post, id) => ({ id, ...post });
+    type Post {
+      id: ID
+      title: String
+      content: String
+    }
+  `);
 
-const root = {
-  posts: () => POSTS.map(mapPost),
-  post: ({ id }) => mapPost(POSTS[id], id),
+  const mapPost = (post, id) => ({ id, ...post });
+
+  const root = {
+    posts: () => POSTS.map(mapPost),
+    post: ({ id }) => mapPost(POSTS[id], id),
+  };
+
+  // GraphQL middleware magic
+  app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }));
+
+  // Port/server creation
+  const PORT = process.env.PORT || 5309;
+  app.listen(PORT, () => console.log(`\n.: The server is running on port: ${PORT} :.\n`));
 };
 
-// GraphQL middleware magic
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true
-}));
-
-// Port/server creation
-const PORT = process.env.PORT || 5309;
-app.listen(PORT, () => console.log(`\n.: The server is running on port: ${PORT} :.\n`));
+startServer();
