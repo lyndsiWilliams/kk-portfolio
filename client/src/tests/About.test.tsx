bfd7d9cbbc7fa99063f163bb5fc3e9dc84738083
lyndsiWilliams <kcatgirl@gmail.com>
2021-03-08 16:07:35 -0600

    Added base test file for About component

---
+++ client/src/tests/About.test.tsx
@@ -0,0 +1,8 @@
+// Package imports
+import { render } from '@testing-library/react';
+// Component
+import About from '../components/About';
+
+it('renders the About component', () => {
+  render(<About />);
+});
