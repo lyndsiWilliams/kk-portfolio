b5c1dc760cee309818230cce82a36096f99a6ffc
lyndsiWilliams <kcatgirl@gmail.com>
2021-03-10 16:07:35 -0600

    Added base test file for Gallery component

---
+++ client/src/tests/Gallery.test.tsx
@@ -0,0 +1,8 @@
+// Package imports
+import { render } from '@testing-library/react';
+// Component
+import Gallery from '../components/Gallery';
+
+it('renders the Gallery component', () => {
+  render(<Gallery />);
+});
