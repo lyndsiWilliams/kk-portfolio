acf6acfc3ca6e1c0c9cb9489b0a4541681bdac98
lyndsiWilliams <kcatgirl@gmail.com>
2021-03-12 16:07:35 -0600

    Added base test file for Home component

---
+++ client/src/tests/Home.test.tsx
@@ -0,0 +1,8 @@
+// Package imports
+import { render } from '@testing-library/react';
+// Component
+import Home from '../components/Home';
+
+it('renders the Home component', () => {
+  render(<Home />);
+});
