3dcc4f764cb25f5e03f20913fdba21037f9423a6
lyndsiWilliams <kcatgirl@gmail.com>
2021-03-14 16:07:35 -0600

    Added base test file for ShortStories component

---
+++ client/src/tests/ShortStories.test.tsx
@@ -0,0 +1,8 @@
+// Package imports
+import { render } from '@testing-library/react';
+// Component
+import ShortStories from '../components/ShortStories';
+
+it('renders the ShortStories component', () => {
+  render(<ShortStories />);
+});
