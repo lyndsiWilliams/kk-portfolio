c396d5a08255c1fac6841aaa0ac954bd9da0e535
lyndsiWilliams <kcatgirl@gmail.com>
2021-03-15 16:07:35 -0600

    Testing complete for About component

--- client/src/tests/About.test.tsx
+++ client/src/tests/About.test.tsx
@@ -1,8 +1,18 @@
 // Package imports
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 // Component
 import About from '../components/About';
 
-it('renders the About component', () => {
-  render(<About />);
+describe('About component', () => {
+  it('renders the About component', () => {
+    render(<About />);
+  });
+  it('displays placeholder text', () => {
+    render(<About />);
+    
+    const placeholderText = screen.getByRole('heading', {
+      name: /this is the about page!/i
+    });
+    expect(placeholderText).toBeVisible();
+  });
 });
