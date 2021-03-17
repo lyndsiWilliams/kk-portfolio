d541214cfc278776e258753ee8085167a0c49136
lyndsiWilliams <kcatgirl@gmail.com>
2021-03-17 16:07:35 -0600

    Testing complete for Gallery component

--- client/src/tests/Gallery.test.tsx
+++ client/src/tests/Gallery.test.tsx
@@ -1,8 +1,18 @@
 // Package imports
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 // Component
 import Gallery from '../components/Gallery';
 
-it('renders the Gallery component', () => {
-  render(<Gallery />);
+describe('Gallery component', () => {
+  it('renders the Gallery component', () => {
+    render(<Gallery />);
+  });
+  it('displays placeholder text', () => {
+    render(<Gallery />);
+    
+    const placeholderText = screen.getByRole('heading', {
+      name: /this is the gallery page!/i
+    });
+    expect(placeholderText).toBeVisible();
+  });
 });
