812edac235234917db9b7d6d6758c74710d5c7f3
lyndsiWilliams <kcatgirl@gmail.com>
2021-03-29 16:07:35 -0600

    Added OS detection

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,8 +1,11 @@
+import { osName } from 'react-device-detect';
+
 const Home = () => {
   return (
     <div className="home">
       <h1>Kathleen Kidder</h1>
       <h2>Writer, artist, cat and lizard fanatic</h2>
+      <p>Your operating system is {osName}.</p>
     </div>
   );
 };
