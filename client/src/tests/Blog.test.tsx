19a872a30e7f0b479a5e80d1d555a00f38517629
lyndsiWilliams <kcatgirl@gmail.com>
2021-04-03 16:07:35 -0600

    Took GQL out of Blog tests

--- client/src/tests/Blog.test.tsx
+++ client/src/tests/Blog.test.tsx
@@ -1,13 +1,8 @@
 // Package imports
-import { ApolloProvider } from '@apollo/client';
 import { render } from '@testing-library/react';
 // Component
 import Blog from '../components/Blog';
-import client from '../apollo';
 
 it('renders the Blog component', () => {
-  render(
-    <ApolloProvider client={client}>
-      <Blog />
-    </ApolloProvider>);
+  render(<Blog />)
 });
