da1f613967d07bb6d01b3151c627e060b314d274
lyndsiWilliams <kcatgirl@gmail.com>
2021-04-18 16:07:35 -0600

    Created skeleton blog form component

---
+++ client/src/components/Blog/BlogForm.tsx
@@ -0,0 +1,12 @@
+// Package imports
+import axios from 'axios';
+
+const BlogForm = () => {
+  return (
+    <div className="blog-form">
+      This is the BlogForm.
+    </div>
+  );
+};
+
+export default BlogForm;
