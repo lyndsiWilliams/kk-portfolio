634d85402fbad80f8737a481898715c57d26e815
lyndsiWilliams <kcatgirl@gmail.com>
2021-04-23 16:07:35 -0600

    Starting to structure blog POST form

--- client/src/components/Blog/BlogForm.tsx
+++ client/src/components/Blog/BlogForm.tsx
@@ -3,10 +3,96 @@ import axios from 'axios';
 // Styling
 import { Button, Modal, Form, Input, Radio } from 'antd';
 
-const BlogForm = () => {
+interface Values {
+  title: string;
+  description: string;
+  modifier: string;
+}
+
+interface BlogFormProps {
+  visible: boolean;
+  onCreate: (values: Values) => void;
+  onCancel: () => void;
+}
+
+const BlogForm: React.FC<BlogFormProps> = ({
+  visible,
+  onCreate,
+  onCancel,
+}) => {
+  const [form] = Form.useForm();
+
   return (
-    <div className="blog-form">
-      This is the BlogForm.
+    <Modal
+      visible={visible}
+      title="Create a new blog post"
+      okText="Create"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(values => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch(info => {
+            console.log('Validate Failed:', info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{ modifier: 'public' }}
+      >
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[{ required: true, message: 'Please input the title of this blog post.' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="description" label="Description">
+          <Input type="textarea" />
+        </Form.Item>
+        <Form.Item name="modifier" style={{ marginBottom="0" }}>
+          <Radio.Group>
+            <Radio value="public">Public</Radio>
+            <Radio value="private">Private</Radio>
+          </Radio.Group>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+const CollectionsPage = () => {
+  const [visible, setVisible] = useState(false);
+
+  const onCreate = (values: any) => {
+    console.log('Received values of form: ', values);
+    setVisible(false);
+  };
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        New Collection
+      </Button>
+      <BlogForm
+        visible={visible}
+        onCreate={onCreate}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      />
     </div>
   );
 };
