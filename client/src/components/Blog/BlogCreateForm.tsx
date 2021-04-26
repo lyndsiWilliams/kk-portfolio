aa02aefc7a534f13dd9b88ed899fddacd208fcac
lyndsiWilliams <kcatgirl@gmail.com>
2021-04-26 16:07:35 -0600

    Moving some things around

---
+++ client/src/components/Blog/BlogCreateForm.tsx
@@ -0,0 +1,69 @@
+import { Modal, Form, Input, Radio } from 'antd';
+
+
+interface Values {
+  title: string;
+  description: string;
+  modifier: string;
+}
+
+interface BlogCreateFormProps {
+  visible: boolean;
+  onCreate: (values: Values) => void;
+  onCancel: () => void;
+}
+
+const BlogCreateForm: React.FC<BlogCreateFormProps> = ({
+  visible,
+  onCreate,
+  onCancel,
+}) => {
+  const [form] = Form.useForm();
+
+  return (
+    <Modal
+      visible={visible}
+      title="Create a new blog post"
+      okText="Create"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(values => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch(info => {
+            console.log('Validate Failed:', info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{ modifier: 'public' }}
+      >
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[{ required: true, message: 'Please input the title of this blog post.' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="description" label="Description">
+          <Input type="textarea" />
+        </Form.Item>
+        <Form.Item name="modifier" style={{ marginBottom: "0" }}>
+          <Radio.Group>
+            <Radio value="public">Public</Radio>
+            <Radio value="private">Private</Radio>
+          </Radio.Group>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default BlogCreateForm;
